da580c823b8ef8dc1210276bfa0e8f1aa46613b0
Gargi Chakraborty <101615476+Gargi008@users.noreply.github.com>
2022-09-12 18:58:58 +0530

    Add files via upload

--- Javascript/Day30/script.js
+++ Javascript/Day30/script.js
@@ -12,3 +12,73 @@ const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 
+//play and pause feature
+
+
+function togglePlayPause(){
+  if(video.paused){
+    video.play()
+    playBtn.classList.replace("fa-play","fa-pause")
+    playBtn.setAttribute("title","pause")
+  }
+  else{
+    video.pause()
+    playBtn.classList.replace('fa-pause','fa-play')
+  playBtn.setAttribute("title","play")
+    
+  }
+}
+
+//progress bar
+
+function displayTime(time){
+  const minutes=Math.floor(time/60)
+  const seconds=Math.floor(time%60)
+  return `${minutes}:${seconds}`
+}
+
+function updatePogressBar(){
+  progressBar.style.width=`${(video.currentTime/video.duration)*100}%`
+  currentTime.textContent=`${displayTime(video.currentTime)}`
+  duration.textContent=`${displayTime(video.duration)}`
+
+}
+
+function seekProgress(e){
+  const seekTime=e.offsetX/progressRange.clientWidth
+  progressBar.style.width=`${seekTime*100}%`
+  video.currentTime=seekTime * video.duration
+
+}
+
+
+function audioSeeker(event){
+
+  let volume=event.offsetX/volumeRange.offsetWidth
+   if(volume<0.1){
+    volume=0
+   }
+   if(volume>0.9){
+    volume=1
+   }
+
+  volumeBar.style.width=`${volume*100}%`
+  video.volume=volume
+
+}
+
+
+playBtn.addEventListener("click",togglePlayPause)
+video.addEventListener("click",togglePlayPause)
+video.addEventListener("timeupdate",updatePogressBar)
+progressRange.addEventListener("click",seekProgress)
+
+volumeRange.addEventListener("click",audioSeeker)
+
+
+
+
+
+
+
+
